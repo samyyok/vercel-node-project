1b5a94218b3be554307efc0abb9b193c6e12f902
samyyok <samyoksamba17@gmail.com>
2023-11-03 13:54:31 +0100

    recent update

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
 const express = require('express')
 const app = express()
 const PORT = 4000
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
+
+
+
+
+var registerRouter = require('./routes/register');
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(PORT,()=>{
   console.log(`API listening on PORT ${PORT} `)
@@ -14,5 +34,32 @@ app.get('',(req,res)=>{
 app.get('/about',(req,res)=>{
   res.send('This is my about route.... ')
 })
+
+
+app.use((req, res , next)=>{
+  const timestamp = new Date()
+  req.timestamp = timestamp.toString()
+  next()
+})
+
+
+app.use('/register', registerRouter);
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
 module.exports = app
---
+++ routes/register.js
@@ -0,0 +1,88 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/',(req,res)=>{
+  res.render('index',{title:'Porokmi'});
+})
+const profiles = {
+  syok:{
+    image:'/images/luffy.jpg',
+    name:'samyok samba',
+    company:'self',
+    languages:['javascript','python']
+
+  },
+  emusk:{
+    image:'/images/emusk.jpg',
+    name:'elon musk',
+    company:['tesla','SpaceX','Boring'],
+    languages:['python','C++']
+    
+  },
+  DG:{
+    image:'/images/DG.jpg',
+    name:'David Goggins',
+    company:' US-Marines',
+    languages:['stay hard','stay hard','who is gonna carry the boats,...the logs?']
+    
+  }
+}
+
+
+
+router.get('/:profile/:username',(req, res)=>{
+  const profile = req.params.profile
+  const username = req.params.username
+  const currentprofile = profiles[username]
+  
+
+  if (currentprofile == null){
+    res.json({
+      confirmation:'fail',
+      message:'Profile ' + username + ' not found'
+    })
+
+    return
+  }
+
+  currentprofile.timestamp = req.timestamp
+  res.render('profile',currentprofile)
+} )
+
+router.post('/addprofile',(req,res)=>{
+  const body = req.body
+  body.languages = req.body.languages.split(', ')
+
+  profiles[body.username] = body
+  res.redirect('/profile/'+body.username)
+
+  res.json({
+    confirmation:'success',
+    data:body
+  })
+
+})
+
+router.get('/profiles',(req,res)=>{
+  const keys = Object.keys(profiles);
+  const list = []
+  keys.forEach(key => {
+    list.push(key)
+
+  })
+ 
+  const data = {
+    profileshere:list,
+    timestamp:req.timestamp
+    
+  }
+
+  
+  res.render('profiles',data)
+  
+
+})
+
+
+
+module.exports = router;
